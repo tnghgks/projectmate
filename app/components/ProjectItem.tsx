2c26df155c2e6b6e4136cbc0f28ee586184db7f1
tnghgks <tnghgks@naver.com>
2023-05-15 01:38:17 +0900

    style: ProgectItem UI 변경

--- app/components/ProjectItem.tsx
+++ app/components/ProjectItem.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { HamburgerIcon, BellIcon, AddIcon, StarIcon } from "@chakra-ui/icons";
+import { BellIcon, AddIcon, StarIcon } from "@chakra-ui/icons";
 import {
   Card,
   CardHeader,
@@ -11,60 +11,38 @@ import {
   Heading,
   IconButton,
   Text,
-  Image,
-  Button,
 } from "@chakra-ui/react";
 
 export default function ProjectItem() {
   return (
-    <Card maxW="md">
-      <CardHeader>
+    <Card maxW="md" as="article">
+      <CardHeader as="header">
         <Flex>
           <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
             <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
             <Box>
-              <Heading size="sm">Segun Adebayo</Heading>
-              <Text>Creator, Chakra UI</Text>
+              <Heading size="sm" as="h3">
+                프로젝트 이름
+              </Heading>
+              <Text>모집자 이름</Text>
             </Box>
           </Flex>
-          <IconButton
-            variant="ghost"
-            colorScheme="gray"
-            aria-label="See menu"
-            icon={<HamburgerIcon />}
-          />
         </Flex>
       </CardHeader>
-      <CardBody>
-        <Text>
-          With Chakra UI, I wanted to sync the speed of development with the speed of design. I
-          wanted the developer to be just as excited as the designer to create a screen.
-        </Text>
-      </CardBody>
-      <Image
-        objectFit="cover"
-        src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-        alt="Chakra UI"
-      />
-
+      <CardBody>프로젝트 설명</CardBody>
       <CardFooter
         justify="space-between"
         flexWrap="wrap"
+        gap="10px"
         sx={{
           "& > button": {
-            minW: "136px",
+            w: "50px",
           },
         }}
       >
-        <Button flex="1" variant="ghost" leftIcon={<AddIcon />}>
-          Join
-        </Button>
-        <Button flex="1" variant="ghost" leftIcon={<BellIcon />}>
-          Subscribe
-        </Button>
-        <Button flex="1" variant="ghost" leftIcon={<StarIcon />}>
-          Star
-        </Button>
+        <IconButton aria-label="Join" icon={<AddIcon />}></IconButton>
+        <IconButton aria-label="Subscribe" icon={<BellIcon />}></IconButton>
+        <IconButton aria-label="Star" icon={<StarIcon />}></IconButton>
       </CardFooter>
     </Card>
   );
